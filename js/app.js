8b33fa19c4c0df8fd3f7b250167e2f3a79027f28
gnzandrs <gavr@outlook.cl>
2017-07-22 18:19:18 -0400

    offCanvas menu for movil first

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
 $(document).foundation()
 
-$('[data-toggle-dia]').on('click', function (ev) {
+$('[data-toggle-dia]').on('click', function (e) {
 	var panel = $(this).data('toggleDia')
 	$('#lineup-tabs').foundation('selectTab', panel)
 })
+
+
+var $offCanvas = $('#offCanvas')
+var $sticky = $('#sticky')
+
+$offCanvas.find('li').on('click', function (e) {
+	$offCanvas.foundation('close')
+	/* fix left menu animation */
+	setTimeout(function () {
+		$sticky.css('left', '0px')
+	}, 300)
+
+})
+
